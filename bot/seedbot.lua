22a83a7ae4c7c9f472b460d7917a67de305b6472
glsx151 <aligls80@yahoo.com>
2016-04-21 19:02:08 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {215828802},--Sudo users
+    sudo_users = {20206877},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
